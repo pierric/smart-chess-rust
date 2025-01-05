31522b5fa6849cfe3049aeb7e3d01e71bb2ea576
Jiasen Wu <jiasenwu@hotmail.com>
2025-01-05 08:32:23 +0100

    validate the move space

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use ndarray::{Array1, Ix1};
 use numpy::array::{PyArray1, PyArray3};
 use pyo3::prelude::*;
+use std::collections::{HashMap, HashSet};
 
 pub mod chess;
 pub mod game;
@@ -20,7 +21,7 @@ fn encode_move(turn: chess::Color, mov: chess::Move) -> PyResult<i32> {
 
 #[pyfunction]
 fn encode_steps(
-    steps: Vec<(chess::Move, Vec<u32>)>, apply_mirror: bool
+    steps: Vec<(chess::Move, Vec<(chess::Move, u32)>)>, apply_mirror: bool
 ) -> PyResult<Vec<(PyObject, PyObject, PyObject, PyObject)>> {
     let mut history = chess::BoardHistory::new(game::LOOKBACK);
     let mut board_state = chess::BoardState::new();
@@ -31,11 +32,22 @@ fn encode_steps(
 
     Python::with_gil(|py| {
         for (mov, num_act) in steps.iter() {
+            let num_act_dict: HashMap<chess::Move, u32> = num_act.iter().cloned().collect();
             let step = if apply_mirror {
                 board_state.to_board().rotate()
             } else {
                 board_state.to_board()
             };
+
+            // rotate the move if black
+            let rotate_and_encode = |m: &chess::Move| -> i32 {
+                if step.turn == flip_color {
+                    m.rotate().encode()
+                } else {
+                    m.encode()
+                }
+            };
+
             let legal_moves = board_state.legal_moves();
             // latter boards stay at the front
             history.push_front(&step);
@@ -43,26 +55,27 @@ fn encode_steps(
             let encoded_boards = history.view(step.turn == flip_color);
             let encoded_meta = step.encode_meta();
 
-            // rotate the move if black
-            let moves_indices: Vec<i32> = legal_moves
-                .iter()
-                .map(|m| {
-                    if step.turn == flip_color {
-                        m.rotate().encode()
-                    } else {
-                        m.encode()
-                    }
-                })
-                .collect();
-            let sum_num_act: u32 = num_act.iter().sum();
-            let moves_values: Vec<f32> = num_act
-                .iter()
-                .map(|v| *v as f32 / (sum_num_act as f32 + 1e-5))
-                .collect();
+            let moves_indices: Vec<i32> = legal_moves.iter().map(rotate_and_encode).collect();
 
+            // sanity check of the input matching the legal moves
+            let set_moves_in: HashSet<chess::Move> = num_act_dict.keys().cloned().collect();
+            let set_moves_exp: HashSet<chess::Move> = legal_moves.into_iter().collect();
+
+            let diff: Vec<_> = set_moves_in.symmetric_difference(&set_moves_exp).collect();
+            if diff.len() > 0 {
+                for x in diff {
+                    println!("{x}");
+                }
+                panic!("inconsistent moves");
+            }
+
+            let sum_num_act: u32 = num_act_dict.values().sum();
             let mut encoded_dist = Array1::<f32>::zeros((8 * 8 * 73,));
-            for (k, v) in moves_indices.iter().zip(moves_values.into_iter()) {
-                encoded_dist[Ix1(*k as usize)] = v;
+
+            for (mov, cnt) in &num_act_dict {
+                let ind = rotate_and_encode(mov);
+                let val = *cnt as f32 / (sum_num_act as f32 + 1e-5);
+                encoded_dist[Ix1(ind as usize)] = val;
             }
 
             let encoded_boards: &PyArray3<i32> = PyArray3::from_array(py, &encoded_boards);
