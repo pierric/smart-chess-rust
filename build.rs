7fb837594abe9444ccd6ee96c2ec000e82080e71
Jiasen Wu <jiasenwu@hotmail.com>
2025-02-09 07:18:36 +0100

    update build.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
+use std::path::PathBuf;
+
 fn main() {
-    if let Some(lib_path) = std::env::var_os("DEP_TCH_LIBTORCH_LIB") {
-        println!("cargo:rustc-link-arg=-Wl,-rpath={}", lib_path.to_string_lossy());
+    if let Some(lib_path) = std::env::var_os("LIBTORCH") {
+        let p = PathBuf::from(lib_path).join("lib");
+        println!("cargo:rustc-link-arg=-Wl,-rpath={}", p.to_string_lossy());
     }
     println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
     println!("cargo:rustc-link-arg=-ltorch");
