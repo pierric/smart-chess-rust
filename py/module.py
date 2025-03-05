5201fc9bcd22614d498b2fe200b229bfd83066df
Jiasen Wu <jiasenwu@hotmail.com>
2025-03-05 04:10:36 +0100

    minor refactor to use other meta data

--- py/module.py
+++ py/module.py
@@ -26,14 +26,18 @@ class ResBlock(torch.nn.Module):
 
 
 class ChessModule(torch.nn.Module):
-    def __init__(self, n_res_blocks=19):
+    def __init__(self, n_res_blocks=19, n_boards=8):
         super().__init__()
 
+        # NOTE: The encoded boards (without the 7 meta planes) are passed through convoluational
+        # layers. The resulting latent is then passed to the value and policy head. There
+        # the 7 meta floats (meta planes are just a broadcast of these 7 floats to a 7x8x8 shape)
+        # are concatenated to the latent before feeding into the Linear layers.
+
         # 8 boards (14 channels each)
-        # meta (7 channels)
         self.conv_block = torch.nn.Sequential(
             torch.nn.Conv2d(
-                14 * 8, 256, kernel_size=5, stride=1, padding=2, bias=False
+                14 * n_boards, 256, kernel_size=5, stride=1, padding=2, bias=False
             ),
             torch.nn.BatchNorm2d(256),
             torch.nn.ReLU(inplace=False),
@@ -41,46 +45,52 @@ class ChessModule(torch.nn.Module):
 
         self.res_blocks = torch.nn.ModuleList([ResBlock() for _ in range(n_res_blocks)])
 
-        self.value_head = torch.nn.Sequential(
-            torch.nn.Conv2d(256, 16, kernel_size=1, bias=False),
-            torch.nn.BatchNorm2d(16),
-            torch.nn.Flatten(),
-            torch.nn.Linear(1024, 256),
-            torch.nn.ReLU(inplace=False),
-            torch.nn.Linear(256, 1),
-            torch.nn.Tanh(),
-            # torch.nn.Conv2d(256, 64, kernel_size=1, bias=False),
-            # torch.nn.BatchNorm2d(64),
-            # # # torch.nn.AvgPool2d(kernel_size=8),
-            # torch.nn.Dropout2d(p=0.5),
-            # torch.nn.Flatten(),
-            # torch.nn.Linear(64 * 8 * 8, 512),
-            # torch.nn.ReLU(inplace=False),
-            # torch.nn.Linear(512, 1),
-            # torch.nn.Tanh(),
-        )
+        self.value_head_conv = torch.nn.Conv2d(256, 16, kernel_size=1, bias=False)
+        self.value_head_bn = torch.nn.BatchNorm2d(16)
+        # 7 floats of meta information are concatenated
+        self.value_head_linear1 = torch.nn.Linear(8 * 8 * 16 + 7, 256)
+        self.value_head_linear2 = torch.nn.Linear(256, 1)
 
-        self.policy_head = torch.nn.Sequential(
-            torch.nn.Conv2d(256, 128, kernel_size=1, bias=False),
-            torch.nn.BatchNorm2d(128),
-            torch.nn.Flatten(),
-            # torch.nn.Dropout(p=0.5),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(8 * 8 * 128, 8 * 8 * 73),
-        )
+        self.policy_head_conv = torch.nn.Conv2d(256, 128, kernel_size=1, bias=False)
+        self.policy_head_bn = torch.nn.BatchNorm2d(128)
+        self.policy_head_linear = torch.nn.Linear(8 * 8 * 128 + 7, 8 * 8 * 73)
+
+    def value_head(self, x, meta):
+        x = self.value_head_conv(x)
+        x = self.value_head_bn(x)
+        x = torch.flatten(x, 1, -1)
+        x = torch.concatenate((x, meta), dim=1)
+        x = self.value_head_linear1(x)
+        x = torch.nn.functional.relu(x)
+        x = self.value_head_linear2(x)
+        x = torch.nn.functional.tanh(x)
+
+        turn = meta[:, 0].unsqueeze(-1)
+        return x * (turn * 2 - 1)
+
+    def policy_head(self, x, meta):
+        x = self.policy_head_conv(x)
+        x = self.policy_head_bn(x)
+        x = torch.flatten(x, 1, -1)
+        x = torch.concatenate((x, meta), dim=1)
+        x = torch.nn.functional.relu(x)
+        x = self.policy_head_linear(x)
+        return torch.log_softmax(x, dim=1)
 
     def forward(self, inp):
-        x = self.conv_block(inp[:, :112, ...])
+        # inp shape bx119x8x8
+        # dim 0:112 are encoded boards
+        # dim 112:119 are the meta data
+        meta = inp[:, 112:, 0, 0]
+        inp = inp[:, :112, ...]
+
+        x = self.conv_block(inp)
 
         for block in self.res_blocks:
             x = block(x)
 
-        v1 = self.policy_head(x)
-        v1 = torch.log_softmax(v1, dim=1)
-
-        v2 = self.value_head(x)
-        turn = inp[:, 112, 0, 0].unsqueeze(-1)
-        v2 = v2 * (turn * 2 - 1)
+        v1 = self.policy_head(x, meta)
+        v2 = self.value_head(x, meta)
         return v1, v2
 
 
