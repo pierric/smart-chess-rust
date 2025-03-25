1279b6b337112f5d7cf83130ffac42c80abe678f
Jiasen Wu <jiasenwu@hotmail.com>
2025-03-25 15:08:54 +0100

    bugfix in grpc server

--- scripts/serve.py
+++ scripts/serve.py
@@ -7,6 +7,7 @@ import torch
 import numpy as np
 
 from module import load_model
+from torch._prims_common import check
 
 
 class ChessInput(BaseDoc):
@@ -23,23 +24,34 @@ class ChessExecutor(Executor):
     def __init__(self, n_res_blocks, checkpoint, device, **kwargs):
         super().__init__(**kwargs)
         self.device = device
-        self.model = load_model(
-            n_res_blocks=n_res_blocks,
-            device=device,
-            checkpoint=checkpoint,
-            inference=True,
-            compile=True,
-        )
+        if checkpoint.endswith(".ckpt"):
+            self.model = load_model(
+                n_res_blocks=n_res_blocks,
+                device=device,
+                checkpoint=checkpoint,
+                inference=True,
+                compile=True,
+            )
+        elif checkpoint.endswith(".pt2"):
+            self.model = torch._inductor.aoti_load_package(checkpoint)
+        elif checkpoint.endswith(".pt"):
+            self.model = torch.jit.load(checkpoint)
+        else:
+            raise RuntimeError(f"Unsupported checkpoint {checkpoint}")
         print("model loaded.")
 
     @requests(on="/infer")
-    @dynamic_batching(preferred_batch_size=12, timeout=10)
+    @dynamic_batching(preferred_batch_size=8, timeout=2)
     def infer(self, docs: DocList[ChessInput], **kwargs) -> DocList[ChessOutput]:
         tensors = []
 
-        for doc in in docs:
-            b = torch.from_numpy(doc.boards.astype(np.float32)).transpose(2, 0, 1)
-            m = torch.from_numpy(doc.meta.astype(np.float32)).repeat_interleaved(64).reshape(7, 8, 8)
+        for doc in docs:
+            b = torch.from_numpy(doc.boards.astype(np.float32)).permute((2, 0, 1))
+            m = (
+                torch.from_numpy(doc.meta.astype(np.float32))
+                .repeat_interleave(64)
+                .reshape(7, 8, 8)
+            )
             tensors.append(torch.cat((b, m)))
 
         inp = torch.stack(tensors).to(device=self.device)
@@ -50,8 +62,8 @@ class ChessExecutor(Executor):
             ):
                 action, score = self.model(inp)
 
-        action = list(action.cpu().numpy())
-        score = score.cpu().numpy().flatten().tolist()
+        action = list(action.to(torch.float).cpu().numpy())
+        score = score.to(torch.float).cpu().numpy().flatten().tolist()
 
         return DocList[ChessOutput](
             ChessOutput(action=a, score=s) for a, s in zip(action, score)
